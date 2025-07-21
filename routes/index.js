d35d499883aeaa2236091860a33645ed867e6906
nuclear.potato <dima.shved.06@gmail.com>
2025-07-22 02:21:04 +0300

    relationships route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const collectionRouter = require('./collectionRoutes');
+const relationshipRouter = require('./relationshipRoutes');
 
 router.use('/collections', collectionRouter);
+router.use('/relationships', relationshipRouter);
 
 router.use((req, res) => {
     res.status(404).json({ error: 'route undefined' });
